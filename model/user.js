12f9efcd0e31b83680f1a51c5812b794e69ec1d1
phursang <psherpa592@gmail.com>
2020-03-10 16:37:48 +0530

    added user model

---
+++ model/user.js
@@ -0,0 +1,86 @@
+const mongoose = require('mongoose');
+let schema = mongoose.Schema;
+
+let userSchema = new schema(
+    {
+        userId: {
+            type: String,
+            index: true,
+            unique: true
+        },
+        firstName: {
+            type: String,
+            default: ''
+        },
+        lastName: {
+            type: String,
+            default: ''
+        },
+        countryName: {
+            type: String,
+            default: ''
+        },
+        mobileNumber: {
+            type: String,
+            default: ''
+        },
+        password: {
+            type: String,
+            default: ''
+        },
+        email: {
+            type: String,
+            default: ''
+        },
+
+        //storing the ids of all friends
+        friends: {
+            type: [{
+                friendId: {
+                    type: String,
+                    default: ''
+                }
+
+            }],
+        },
+
+        //storing the ids of friends sending friend request
+        friendRequestRecieved: {
+            type: [{
+                friendId: {
+                    type: String,
+                    default: ''
+                },
+
+                friendName: {
+                    type: String,
+                    default: ''
+                },
+
+            }],
+        },
+
+        //storing the ids of friends to whom friend request has been sent
+        friendRequestSent: {
+            type: [{
+                friendId: {
+                    type: String,
+                    default: ''
+                },
+            }],
+        },
+
+        createdOn: {
+            type: Date,
+            default: ""
+        },
+
+        modifiedOn:{
+            type:Date,
+            default:""
+        }
+
+    }
+)
+
+mongoose.model('userModel',userSchema);
