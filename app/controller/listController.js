570803e8d414796797b2f67b23d9d573d7f6a1c7
phursang <psherpa592@gmail.com>
2020-03-12 18:16:15 +0530

    added functionality for getting all the todolist of a user

--- app/controller/listController.js
+++ app/controller/listController.js
@@ -66,6 +66,87 @@ let createList = (req, res) => {
 
 }//end of creating new list
 
+//controller to edit list
+let editList = (req, res) => {
+
+    //checking if list with provided list id is present or not
+    let findListDetails = () => {
+        return new Promise((resolve, reject) => {
+            listModel.findOne({ 'listId': req.params.listId })
+                .exec((err, ListDetails) => {
+                    if (err) {
+                        console.log(err)
+                        logger.error(err.message, 'List Controller: findListDetails', 10)
+                        apiResponse = response.generate(true, 'Failed To Find List Details', 500, null)
+                        reject(apiResponse)
+                    } else if (checkLib.isEmpty(ListDetails)) {
+                        logger.info('No List Found', 'List  Controller:findListDetails')
+                        apiResponse = response.generate(true, 'No List Found', 404, null)
+                        reject(apiResponse)
+                    } else {
+                        resolve(req)
+                    }
+                })
+        })
+    }// end findListdetails
+
+    //now edit list
+    let editListAfterValidation = () => {
+        return new Promise((resolve, reject) => {
+            let options = req.body;
+            options.listModifiedOn = timeLib.now();
+            options.listModifierId = req.user.userId;//userid of modifier
+
+            listModel.update({ 'listId': req.params.listId }, options, (err, res) => {
+                if (err) {
+                    logger.error('error while updating list', 'ListController:editListAfterValidation', 10);
+                    apiResponse = response.generate(true, 'Error while updating list', 500, null);
+                    reject(apiResponse)
+                }
+                else {
+                    apiResponse = response.generate(false, 'List updated', 200, res);
+                    resolve(apiResponse)
+                }
+            })
+        })
+    }
+
+    //promises
+    findListDetails(req, res)
+        .then(editListAfterValidation)
+        .then((resolve) => {
+            res.send(resolve)
+        })
+        .catch((error) => {
+            res.send(error)
+        })//end of promises
+}//end of edit list controller
+
+//controller function for getting all list
+let getAllListOfUser = (req, res) => {
+    //userid added through authorization middleware
+    listModel.find({ 'listCreatorId': req.user.userId })
+        .select('-_id -__v')
+        .exec((err, result) => {
+            if (err) {
+                logger.error(true, 'ListController:getAllListOfUser', 10)
+                apiResponse = response.generate(true, 'failed to find list of user', 500, null);
+                res.send(apiResponse)
+            }
+            else if (checkLib.isEmpty(res)) {
+                logger.info('no list found', 'ListController:getAllListOfUser', 1);
+                apiResponse = response.generate(true, 'No list Found', 404, result);
+                res.send(apiResponse)
+            }
+            else {
+                apiResponse = response.generate(false, 'All list of user found', 200, result);
+                res.send(apiResponse)
+            }
+        })
+}
+
 module.exports = {
-    createList
+    createList,
+    editList,
+    getAllListOfUser
 }
--- app/routes/listRoute.js
+++ app/routes/listRoute.js
@@ -8,6 +8,14 @@ let setRouter = (app)=>
 
     //route to create list
     app.post(`${baseUrl}/create`,authMiddleware.isAuthorized, listController.createList);
+
+    //edit route for list
+    //body param--listTitle
+    app.put(`${baseUrl}/:listId/edit`,authMiddleware.isAuthorized,listController.editList);
+
+    //route for getting all the list of a user
+    //only params is auth
+    app.get(`${baseUrl}/view/all`,authMiddleware.isAuthorized,listController.getAllListOfUser)
 }
 
 module.exports={
